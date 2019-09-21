e7b1f182848bffcf86db7a958dcc172cc4684b3b
Tushar Warade <tush301608021992@gmail.com>
2019-09-21 17:58:39 +0530

    Demo to my brother gagan

---
+++ src/main/java/com/api/customapiinsert/CustomApiInsertApplication.java
@@ -0,0 +1,13 @@
+package com.api.customapiinsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomApiInsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomApiInsertApplication.class, args);
+	}
+
+}
